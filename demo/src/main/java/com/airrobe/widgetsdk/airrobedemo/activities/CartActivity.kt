2b52c10b469020c5f7ba84ad14f29113b531f9e5
~ Coffee + Work + Fun ~ <elizhang221@gmail.com>
2022-05-09 17:17:37 -0400

    confirmation page implemented

--- demo/src/main/java/com/airrobe/widgetsdk/airrobedemo/activities/CartActivity.kt
+++ demo/src/main/java/com/airrobe/widgetsdk/airrobedemo/activities/CartActivity.kt
@@ -1,15 +1,19 @@
 package com.airrobe.widgetsdk.airrobedemo.activities
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.airrobe.widgetsdk.airrobedemo.R
 import com.airrobe.widgetsdk.airrobedemo.adapters.CartItemRVAdapter
 import com.airrobe.widgetsdk.airrobedemo.ui.VerticalSpaceItemDecoration
 import com.airrobe.widgetsdk.airrobedemo.utils.SharedPreferenceManager
+import com.airrobe.widgetsdk.airrobedemo.utils.StatusBarTranslucent
 import com.airrobe.widgetsdk.airrobedemo.utils.Utils
 import com.airrobe.widgetsdk.airrobewidget.widgets.AirRobeMultiOptIn
 
@@ -20,6 +24,8 @@ class CartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
+        StatusBarTranslucent.setStatusBarTranslucent(this, false)
+        StatusBarTranslucent.setStatusBarColor(this)
 
         val categories = arrayListOf<String>()
         for (item in SharedPreferenceManager.getCartItems(this)) {
@@ -35,6 +41,17 @@ class CartActivity : AppCompatActivity() {
 
         val rvCartItemAdapter = CartItemRVAdapter(this, SharedPreferenceManager.getCartItems(this))
         rvCartItems.adapter = rvCartItemAdapter
+
+        val etEmail = findViewById<EditText>(R.id.et_email)
+        val rlPlaceOrder = findViewById<RelativeLayout>(R.id.rl_place_order)
+        rlPlaceOrder.setOnTouchListener { view, motionEvent ->
+            if (Utils.touchAnimator(this, view, motionEvent)) {
+                val intent = Intent(this, ConfirmationActivity::class.java)
+                intent.putExtra("email", etEmail.text.toString())
+                startActivity(intent)
+            }
+            true
+        }
 
         val ivBack = findViewById<ImageView>(R.id.iv_back)
         ivBack.setOnTouchListener { view, motionEvent ->
