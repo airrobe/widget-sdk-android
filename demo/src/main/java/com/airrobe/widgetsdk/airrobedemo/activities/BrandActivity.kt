04d7f1615625b84007e99348da3d1656d94b3299
~ Coffee + Work + Fun ~ <elizhang221@gmail.com>
2022-07-28 12:04:34 -0400

    cart button added to all screens in the demo

--- demo/src/main/java/com/airrobe/widgetsdk/airrobedemo/activities/BrandActivity.kt
+++ demo/src/main/java/com/airrobe/widgetsdk/airrobedemo/activities/BrandActivity.kt
@@ -1,5 +1,7 @@
 package com.airrobe.widgetsdk.airrobedemo.activities
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -8,9 +10,13 @@ import com.airrobe.widgetsdk.airrobedemo.R
 import com.airrobe.widgetsdk.airrobedemo.adapters.BrandsRVAdapter
 import com.airrobe.widgetsdk.airrobedemo.configs.Consts
 import com.airrobe.widgetsdk.airrobedemo.ui.VerticalSpaceItemDecoration
+import com.airrobe.widgetsdk.airrobedemo.utils.SharedPreferenceManager
 import com.airrobe.widgetsdk.airrobedemo.utils.StatusBarTranslucent
+import com.airrobe.widgetsdk.airrobedemo.utils.Utils
+import ru.nikartm.support.ImageBadgeView
 
 class BrandActivity : AppCompatActivity() {
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_brand)
@@ -23,5 +29,21 @@ class BrandActivity : AppCompatActivity() {
 
         val rvBrandAdapter = BrandsRVAdapter(this, Consts.brands)
         rvBrand.adapter = rvBrandAdapter
+
+        val ivCart = findViewById<ImageBadgeView>(R.id.iv_cart)
+        ivCart.setOnTouchListener { view, motionEvent ->
+            if (Utils.touchAnimator(this, view, motionEvent)) {
+                val intent = Intent(this, CartActivity::class.java)
+                startActivity(intent)
+            }
+            true
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val items = SharedPreferenceManager.getCartItems(this)
+        val ivCart = findViewById<ImageBadgeView>(R.id.iv_cart)
+        ivCart.badgeValue = items.count()
     }
 }
